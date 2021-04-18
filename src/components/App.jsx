859bb73254d144984a0c7e78bcb68b991968cb7c
darkgraycat <jburyanov@gmail.com>
2021-04-19 02:10:29 +0300

    done with config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Button from './Button.jsx'
 
 const App = () => {
   return (
     <>
+      <Button color="blue" radius="0px" size="200 50">
+        <p>MORE DETAILS</p>
+      </Button>
+      <Button color="red" radius="50%" size="40 40">
+        <i className="fab fa-facebook-f"></i>
+      </Button>
     </>
   )
 }
